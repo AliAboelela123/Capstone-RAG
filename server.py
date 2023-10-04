0e67a6c067b916525d03943bced47af68f8fb17f
Ali Aboelela <ali.k.aboelela@gmail.com>
2023-10-04 17:20:39 -0400

    Subprocesses are blocking. Decided to use multiprocessing instead. One process for the server, one for the app.

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-import subprocess
 from flask import Flask
 from waitress import serve
 
@@ -7,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello, world!"
+
+def start_server():
+    serve(app, host='127.0.0.1', port=5000)
 
 if __name__ == '__main__':
     serve(app, host='127.0.0.1', port=5000)
