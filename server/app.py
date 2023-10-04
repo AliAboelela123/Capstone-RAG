4d9691ac8f920811af3ab49cd4d0eb3beca80155
Ali Aboelela <ali.k.aboelela@gmail.com>
2023-10-04 14:05:58 -0400

    Added server folder, and a sample flask app served by waitress.

---
+++ server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, world!"
+
+if __name__ == '__main__':
+    serve(app, host='127.0.0.1', port=5000)
